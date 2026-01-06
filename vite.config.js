5df89ebed1a8f94172308e8559ae617249d6d27f
adamcsanyi16 <csanyiadam8@gmai.com>
2026-01-06 22:32:21 +0100

    config

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [["babel-plugin-react-compiler"]],
-        base: "/nottingham-biraja/",
-      },
-    }),
-  ],
+  plugins: [react()],
+  base: "/nottingham-biraja/",
 });
