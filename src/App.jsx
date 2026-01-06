9bf7ef90288c85a7736f536807b333c1eec83906
adamcsanyi16 <csanyiadam8@gmai.com>
2026-01-06 23:01:52 +0100

    basename

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
-import './App.css'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import NewGame from './pages/NewGame'
-import Home from './pages/Home'
-import Results from './pages/Results'
-
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NewGame from "./pages/NewGame";
+import Home from "./pages/Home";
+import Results from "./pages/Results";
 
 function App() {
-
   return (
     <div className="main">
-      <Router>
+      <Router basename={import.meta.env.BASE_URL}>
         <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/felvetel" element={<NewGame/>} />
-          <Route path="/results" element={<Results/>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/felvetel" element={<NewGame />} />
+          <Route path="/results" element={<Results />} />
         </Routes>
       </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
